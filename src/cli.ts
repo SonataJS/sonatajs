ccdb16e81cfd71ab8277f7c449c8799b521ee9a2
Milos Zivlak <zivlakmilos@gmail.com>
2023-07-25 00:46:46 +0200

    Cli help and parse arguments

--- src/cli.ts
+++ src/cli.ts
@@ -4,9 +4,33 @@ import { hideBin } from 'yargs/helpers';
 
 const parseArguments = (rawArgs: string[]) => {
   const args = yargs(hideBin(rawArgs))
-    .usage('Usage: projizi <command> [options]').alias('h', 'help')
+    .usage('Usage: sonata <command> [options]').alias('h', 'help')
     .version().alias('v', 'version')
     .epilog(`Copyright (c) 2023 - ${new Date().getFullYear()} Milos Zivlak`)
+    .command('init [options]', 'Init config file', (yargs) => {
+      return yargs
+        .option('config', {
+          alias: 'c',
+          type: 'string',
+          description: 'Config file',
+          default: 'sonata.json',
+        });
+    })
+    .command('generate [options]', 'Generate projects and files', (yargs) => {
+      return yargs
+        .option('config', {
+          alias: 'c',
+          type: 'string',
+          description: 'Config file',
+          default: 'sonata.json',
+        })
+        .option('output', {
+          alias: 'o',
+          type: 'string',
+          description: 'Output folder',
+          default: './',
+        });
+    })
     .parse();
 
   return args;
@@ -15,4 +39,6 @@ const parseArguments = (rawArgs: string[]) => {
 export const cli = async (rawArgs: string[]) => {
   console.log(figlet.textSync('SonataJS'));
   const args = parseArguments(rawArgs);
+
+  console.log(args);
 }
